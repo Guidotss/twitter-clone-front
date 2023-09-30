289fa70c7c09646c4879325cbb1f38303b73508c
Guidotss <guidoolguin@hotmail.com>
2023-09-30 15:17:21 -0300

    Terminando middleware de auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
 export const middleware = (req: NextRequest) => {
-    if(req.nextUrl.pathname == "/") { 
-        const token = req.cookies.get("token");
-        console.log(token); 
-       if(!token) {
-        NextResponse.redirect(new URL("/auth", req.nextUrl).href); 
-       }
+  if (req.nextUrl.pathname == "/") {
+    const token = req.cookies.get("token");
+    console.log(token);
+    if (!token) {
+      return NextResponse.redirect(new URL("/auth", req.nextUrl).href);
     }
-    return NextResponse.next(); 
-}
+  }
+  return NextResponse.next();
+};
