061c8857f75411899ad63fd223bfbfaac5540975
Guidotss <guidoolguin@hotmail.com>
2023-10-17 15:48:11 -0300

    Empezando tweetPage

---
+++ src/app/tweet/[id]/page.tsx
@@ -0,0 +1,22 @@
+async function GetTweetById(id: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/tweets/${id}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+interface TweetPageProps {
+  params: {
+    id: string;
+  };
+}
+export default async function TweetPage({ params }: TweetPageProps) {
+  const { id } = params;
+  const tweet = await GetTweetById(id);
+  return <h1>holaaa</h1>;
+}
--- src/components/tweets/TweetCard.tsx
+++ src/components/tweets/TweetCard.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { Tweet, User } from "@/interfaces";
-import Image from "next/image";
-import { CommentIcon, LikeIcon, MoreOptionsIcon, RePostIcon } from "..";
 import { useContext } from "react";
+import { useRouter } from 'next/navigation'; 
+import Image from "next/image";
 import { AuthContext, TweetsContext, UiContext } from "@/context";
+import { Tweet, User } from "@/interfaces";
+import { MoreOptionsIcon } from "..";
 import { TweetData } from "./TweetData";
 import { MediaUpload } from "../shared/media";
 import { TweetsActions } from "./TweetsActions";
@@ -15,9 +16,17 @@ interface TweetCardProps {
 }
 
 export const TweetCard = ({ tweet, user }: TweetCardProps) => {
+
+  const router = useRouter();
+
   const { openCommentModal } = useContext(UiContext);
   const { setCurrentTweeet, setLike, setRetweet } = useContext(TweetsContext);
   const { user: currentUser } = useContext(AuthContext);
+
+  const handleNavigation = (e:React.MouseEvent) => {
+    e.stopPropagation(); 
+    router.push(`/tweet/${tweet.id}`);
+  }
 
   const onSetCurrentTweet = () => {
     setCurrentTweeet(tweet.id);
@@ -38,7 +47,7 @@ export const TweetCard = ({ tweet, user }: TweetCardProps) => {
   );
 
   return (
-    <div className="flex gap-3 px-5 py-2 hover:bg-gray-700 hover:bg-opacity-10 transition-colors  duration-300 ease-in-out cursor-pointer">
+    <div className="flex gap-3 px-5 py-2 hover:bg-gray-700 hover:bg-opacity-10 transition-colors  duration-300 ease-in-out cursor-pointer" onClick={(e) => handleNavigation(e)}>
       <figure>
         <Image
           className="rounded-full p-1"
